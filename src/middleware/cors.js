3b4acb3b8d901ed83a2fb00c3069f0cd2f66006c
Aitipamula Anil <aitipamulaanil@gmail.com>
2026-05-21 13:23:13 +0530

    replaced cors

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,8 +1,7 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: ["https://gundusoodhinews.web.app", "http://localhost:3000"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  origin: "*",
   credentials: true,
 });
 
