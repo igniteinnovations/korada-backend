156b6c2d9938f84a29bef18113679ff0f98d4687
Aitipamula Anil <aitipamulaanil@gmail.com>
2026-05-30 16:32:00 +0530

    added only image instead of whole object

--- src/controllers/analytics.controller.js
+++ src/controllers/analytics.controller.js
@@ -258,6 +258,16 @@ export const getAllAnalytics = async (req, res, next) => {
         $unwind: "$article",
       },
       {
+        $project: {
+          articleId: 1,
+          totalViews: 1,
+          totalSessions: 1,
+          averageSessionTime: 1,
+          bounceRate: 1,
+          mediaUrl: "$article.mediaUrl",
+        },
+      },
+      {
         $sort: {
           totalViews: -1,
         },
